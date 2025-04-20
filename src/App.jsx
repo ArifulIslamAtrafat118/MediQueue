61f4f871658fa8010747f6a474af9b9756dd2b88
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-04-20 23:04:41 +0600

    Project setup is done.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl'>Hello project is ready</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1 className='text-8xl text-center'>Project setup is done</h1>
+    </div>
+  )
+}
+
+export default Home
